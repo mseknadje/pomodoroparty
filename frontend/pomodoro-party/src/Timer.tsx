aeceee4aee7f43718f03580ed958db60553011fe
angelay1006 <angela_yeung@brown.edu>
2022-11-30 21:51:42 -0500

    feat: started on 5 min button

--- frontend/pomodoro-party/src/Timer.tsx
+++ frontend/pomodoro-party/src/Timer.tsx
@@ -7,10 +7,12 @@ function startTimer() {
     alert("click")
 }
 
-// src = https://thewebdev.info/2021/01/31/create-a-pomodoro-timer-with-react-and-javascript/ 
-// https://aleksandarpopovic.com/Infinite-Pomodoro-App-in-React/ 
-
-function Timer() {
+/**
+ * Function that instantiates a button for 25 minutes
+ * https://aleksandarpopovic.com/Infinite-Pomodoro-App-in-React/ 
+ * @returns 
+ */
+function Timer25() {
     const [minutes, setMinutes] = useState(25)
     const [seconds, setSeconds] = useState(0)
     const [displayMessage, setDisplayMessage] = useState(false)
@@ -19,7 +21,6 @@ function Timer() {
     function start() {
         // alert("start")
         setStartButton(true)
-
       }
 
     useEffect(() => {
@@ -56,7 +57,60 @@ function Timer() {
       <div className="timer">
         {/* Components that update */}
       </div>
-      <div className="button button1"> 
+      <div className="button-button1"> 
+        <button onClick={start}> {timerMinutes}:{timerSeconds} </button>
+      </div>
+    </div>
+  )
+  
+}
+
+function Timer5() {
+    const [minutes, setMinutes] = useState(5)
+    const [seconds, setSeconds] = useState(0)
+    const [displayMessage, setDisplayMessage] = useState(false)
+    const [startButton, setStartButton] = useState(false)
+
+    function start() {
+        // alert("start")
+        setStartButton(true)
+      }
+
+    useEffect(() => {
+        if (startButton) {
+        let interval = setInterval(() => {
+          clearInterval(interval)
+    
+          if (seconds === 0) {
+            if (minutes !== 0) {
+              setSeconds(59)
+              setMinutes(minutes - 1)
+            } else {
+              let minutes = displayMessage ? 24 : 4
+              let seconds = 59
+    
+              setSeconds(seconds)
+              setMinutes(minutes)
+              setDisplayMessage(!displayMessage)
+            }
+          } else {
+            setSeconds(seconds - 1)
+          }
+        }, 1000)}
+      }, [startButton, seconds]) 
+
+  const timerMinutes = minutes < 10 ? `0${minutes}` : minutes
+  const timerSeconds = seconds < 10 ? `0${seconds}` : seconds
+
+  return (
+    <div className="pomodoro">
+      <div className="message">
+        {displayMessage && <div>Break time! New session starts in:</div>}
+      </div>
+      <div className="timer">
+        {/* Components that update */}
+      </div>
+      <div className="button button2"> 
         <button onClick={start}> {timerMinutes}:{timerSeconds} </button>
       </div>
 
@@ -66,10 +120,10 @@ function Timer() {
 
     </div>
   )
-  
+
 }
 
 
 
 
-export default Timer
+export default Timer25; 
