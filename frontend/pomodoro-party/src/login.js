0ef97dee146dbd8c16928cc4f3f29a2d8fc6d3dc
sabrinachwalek <sabrinachwalek@gmail.comgit config --global user.name Sabrina>
2022-12-01 21:02:38 -0500

    login functionality

---
+++ frontend/pomodoro-party/src/login.js
@@ -0,0 +1,64 @@
+
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signOut } from "firebase/auth";
+
+
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const uid = user.uid;
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+  } else {
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+  }
+});
+
+function login() {
+    var userEmail = document.getElementById("email_field");
+    var userPassword = document.getElementById("password_field");
+
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, userEmail, userPassword)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    });
+
+}
+
+function logout() {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+        // Sign-out successful.
+    }).catch((error) => {
+        // An error happened.
+    });
+
+}
+
+function createAccount() {
+    var userEmail = document.getElementById("email_field");
+    var userPassword = document.getElementById("password_field");
+
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+    });
+}
