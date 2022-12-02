c7f194d58c21450d80cc93bdab5edafb8c14068a
sabrinachwalek <sabrinachwalek@gmail.comgit config --global user.name Sabrina>
2022-12-02 13:28:33 -0500

    minor changes

---
+++ frontend/pomodoro-party/src/firebase_config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDt2DsPS8W08PghNlU2ZBBYGu3-FOOWK4o",
+    authDomain: "pomodoro-party-8e414.firebaseapp.com",
+    projectId: "pomodoro-party-8e414",
+    storageBucket: "pomodoro-party-8e414.appspot.com",
+    messagingSenderId: "453359878600",
+    appId: "1:453359878600:web:f9a9eb37226975477f40fd",
+    measurementId: "G-QSSKPMYF1S"
+}
