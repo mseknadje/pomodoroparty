decf872df4e4f6d4c712a4ef259449e3b5891c83
Maxime Seknadje <maxime@air-de-maxime.devices.brown.edu>
2022-12-09 18:38:58 -0500

    login but not showing up

--- frontend/pomodoro-party/src/App.js
+++ frontend/pomodoro-party/src/App.js
@@ -1,28 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
-
 import './Navbar.css';
 import Navbar from './Navbar';
 import { Party } from './Pages/Party';
 import { Profile } from './Pages/Profile';
 import Todo from './Todo';
-import { Route, Routes} from "react-router-dom";
-import { Box } from "@chakra-ui/react"
-import Main from "./Main"
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { Box } from "@chakra-ui/react";
+import Register from "./Login/Register";
+import Reset from "./Login/Reset";
+import Dashboard from "./Login/Dashboard";
+import Login from './Login/Login';
 
 /**
  * The top-level function that contains routes to each page 
  * @returns 
  */
+// function App() {
+//   return (
+//     <Box className="container" bgColor={"#FFF3F3"}>
+//       <Routes>
+//         <Route path="/profile" element={<Profile />} />
+//         <Route path="/party" element={<Party />} />
+//       </Routes>
+//     </Box>
+//   );
+// }
+
 function App() {
   return (
-    <Box className="container" bgColor={"#FFF3F3"}>
+    <div className="app">
+      {/* <Router> */}
       <Routes>
-        <Route path="/" element={<Main />} />
+      <Route path="/" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/party" element={<Party />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/reset" element={<Reset />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </Box>
+      {/* </Router> */}
+      </div>
   );
 }
 
