b38bfdbbd9b3cb6556aba5c9ad2c4f5e00675211
angelay1006 <angela_yeung@brown.edu>
2022-12-11 16:17:42 -0500

    fixed more todo

--- frontend/pomodoro-party/src/Todo.js
+++ frontend/pomodoro-party/src/Todo.js
@@ -30,32 +30,34 @@ export default function Todo(){
     useEventListener('keydown', handler);
 
 
-    return <Box position="absolute" top="50px" width="313px" height="368px" left="16px" 
-    background="#fff" p="15px">
+    return <Box position="absolute" top="100px" width="313px" minHeight="368px" left="16px" 
+    background="#fff" p="15px" pb="50px">
         <Center>
             <Heading as="h1" padding="2px 10px" bgGradient="linear(to-l, #7928CA,#FF0080)"
              bgClip="text" fontWeight="extrabold" mb="15px">
                 To-Do List
             </Heading>
         </Center>
-        <VStack p={0}> 
+        <VStack p={"0px"} spacing="0px"> 
                  {
                      
                     todoList.map((t,i)=> {
-                        <HStack spacing="24px" w="320px"> 
-                        return <FormControl key={i}> <Checkbox mt="5px" pl="0px" mr="10px"/>
-                        <FormLabel display={"inline-block"} mt="-5px" ml="0px">{t}</FormLabel>
+
+                         
+                        return <FormControl key={i} m="0px"> <Checkbox mt="0px" pl="0px" mr="10px"/>
+                        <FormLabel display={"inline-block"} mt="0px" ml="0px" position={"absolute"} top="-5px">
+                            {t}</FormLabel>
                         </FormControl>
-                        </HStack>
+                       
                     })}
-        
+          </VStack>
         
         <Center position="absolute" bottom="10px" left="0" width={"100%"}>
             <Input border="solid 1px black" borderColor={"black"} height="25px"
                mt="15px" placeholder="add an item" width={"275px"}
                borderRadius={"10px"} value={inputValue} onChange={handleOnChange} /> 
         </Center>
-        </VStack>
+      
     </Box>
 }
 
